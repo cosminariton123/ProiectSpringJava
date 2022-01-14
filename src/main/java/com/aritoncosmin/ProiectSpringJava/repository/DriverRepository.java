327ef8b278dd7295ebdfc6eb1ea51da499ac428b
Darkrider123 <cosmin.ariton@gmail.com>
2022-01-14 03:50:00 +0200

    added repositories

---
+++ src/main/java/com/aritoncosmin/ProiectSpringJava/repository/DriverRepository.java
@@ -0,0 +1,7 @@
+package com.aritoncosmin.ProiectSpringJava.repository;
+
+import com.aritoncosmin.ProiectSpringJava.model.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepository extends JpaRepository<Driver, Integer> {
+}
---
+++ src/main/java/com/aritoncosmin/ProiectSpringJava/repository/ServiceAuto.java
@@ -0,0 +1,6 @@
+package com.aritoncosmin.ProiectSpringJava.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceAuto extends JpaRepository<ServiceAuto, Integer> {
+}
